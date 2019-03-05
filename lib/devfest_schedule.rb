c88286423d49cb7142b7472499cf6a38df0d518f
dclements9 <dclements@protonmail.com>
2019-03-05 08:27:58 -0500

    first commit

---
+++ lib/devfest_schedule.rb
@@ -0,0 +1,6 @@
+require "devfest_schedule/version"
+
+module DevfestSchedule
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/devfest_schedule/version.rb
@@ -0,0 +1,3 @@
+module DevfestSchedule
+  VERSION = "0.1.0"
+end
