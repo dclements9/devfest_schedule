03560a97a543ae4aa0a9b5f2fa2cf70ce6c99310
dylanclements <dclements@protonmail.com>
2019-03-09 07:22:44 -0500

    Speaker, Reworked Title, & Session ID completed

--- current_time_attr_output.md
+++
@@ -1,9 +0,0 @@
-talk.start_time = element.css("time.start-time")
-#Output:
-
-[#<Nokogiri::XML::Element:0x2cda440 name="time" attributes=[#<Nokogiri::XML::Attr:0x2cda3c8 name="class" value="start-time">, #<Nokogiri::XML::Attr:0x2cda3a0 name="itemprop" value="startDate">, #<Nokogiri::XML::Attr:0x2cda378 name="datetime" value="October 27T9:30">] children=[#<Nokogiri::XML::Text:0x2ce3568 " 9">, #<Nokogiri::XML::Element:0x2ce3360 name="span" children=[#<Nokogiri::XML::Text:0x2ce2ca8 "30">]>, #<Nokogiri::XML::Text:0x2ce2a50 " ">]>]
-
-talk.start_time = element.css("time.start-time").attribute("datetime")
-#Output:
-
-(Attr:0x2c2f004 { name = "datetime", value = "October 27T10:30" }),
--- lib/devfest_schedule/talk.rb
+++ lib/devfest_schedule/talk.rb
@@ -2,40 +2,40 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'pry-byebug'
 
 class Talks
-    attr_accessor :title, :speaker, :description, :start_time, :end_time
+    attr_accessor :title, :speaker, :description, :start_time, :end_time, :session_id
     @@all = []
 
     def initialize
         @@all << self
     end
-    #binding.pry
-    def self.scrape_talks
-        schedule = Nokogiri::HTML(open("https://devfest.gdgcapitalregion.com/schedule/"))
-        
-        schedule.css("div.timeslot").each do |element|   
-           # binding.pry       
-            talk = self.new
-            talk.title = element.css("h5.slot-title").text
-            #talk.speaker = element.at_css("p.speaker-name").text
-            talk.speaker = element.css("p.speaker-name").text
 
-            #talk.start_time = element.css("time.start-time")
-            #talk.start_time = element.css("time.start-time").attribute("datetime")
-            talk.start_time = element.css("time.start-time datetime").values
+    def self.scrape_talks   
+        schedule = Nokogiri::HTML(open("https://devfest.gdgcapitalregion.com/schedule/"))
+        schedule.css("div.slot.col-md-4.col-xs-12.flexbox-item-height").each do |element|  
+            talk = self.new
+            if element.css("h5.slot-title").text != ""     
+                talk.title = element.css("h5.slot-title").text
+                talk.speaker = element.css("p.speaker-name").text 
+                if element.attribute('id') != nil
+                    talk.session_id = element.attribute('id').value
+                end
+            end
+            ## Instance tests
             #binding.pry
-        
-            schedule.css("div.modals").each do |descriptions|
-                talk.description = element.css("p.theme-description").text
-        end
-        #binding.pry
-        talk
-    end
-        
-        #Test @@all
+           talk
+        end    
+        ## Test @@all
         binding.pry  
     end
-#Testing method by calling
+## Testing method by calling
 scrape_talks
 end
+
+### Session detail label = same for description && speaker session
+### Times: 
+    # @schedule.css("div.timeslot").each do |element| 
+    # talk.start_time = (element.css("time.start-time").attribute("datetime").value).gsub('T', " ")
+    # talk.end_time = (element.css("time.end-time").attribute("datetime").value).gsub('T', " ")
