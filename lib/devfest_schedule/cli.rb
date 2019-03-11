a10b641259b808af5680e24a1c787ac1ae00850e
dylanclements <dclements@protonmail.com>
2019-03-11 10:52:02 -0400

    Description done. Times next.

--- lib/devfest_schedule/cli.rb
+++ lib/devfest_schedule/cli.rb
@@ -1,6 +1,10 @@
 class DevfestSchedule::CLI
 
     def call
+        welcome
+    end
 
+    def welcome
+        puts "Welcome to the DevFest Schedule CLI"
     end
 end
--- lib/devfest_schedule/talk.rb
+++ lib/devfest_schedule/talk.rb
@@ -2,12 +2,11 @@
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
-require 'pry-byebug'
 
 class Talks
     attr_accessor :title, :speaker, :description, :start_time, :end_time, :session_id
     @@all = []
-
+    
     def initialize
         @@all << self
     end
@@ -20,22 +19,39 @@ class Talks
                 talk.title = element.css("h5.slot-title").text
                 talk.speaker = element.css("p.speaker-name").text 
                 if element.attribute('id') != nil
-                    talk.session_id = element.attribute('id').value
+                    session_id = element.attribute('id').value
+                    ## Grab only digits - Not gsub method
+                    talk.session_id = session_id.gsub(/\D/, "")
                 end
-            end
+             end
+           descriptions_modal = schedule.search("div.modals")
+        
+           descriptions_modal.css("div.modal.fade.text-left.people-modal").each do |modal|
+
+           if modal.attribute('id') != nil
+               ## Need torefactor @@all to class method
+               @@all.each do |object|
+                if description_id = modal.attribute('id').value.gsub(/\D/, "") === talk.session_id
+                    talk.description = modal.css("p.theme-description").text
+                end
+               end
+           end
+       end
             ## Instance tests
             #binding.pry
            talk
         end    
         ## Test @@all
-        binding.pry  
+         binding.pry  
     end
+    
 ## Testing method by calling
 scrape_talks
 end
 
 ### Session detail label = same for description && speaker session
 ### Times: 
+    # schedule.search("")
     # @schedule.css("div.timeslot").each do |element| 
     # talk.start_time = (element.css("time.start-time").attribute("datetime").value).gsub('T', " ")
     # talk.end_time = (element.css("time.end-time").attribute("datetime").value).gsub('T', " ")
