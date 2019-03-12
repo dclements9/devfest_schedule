d1775486ce4c642e15bccfa6fed24e6e640fbf78
dclements9 <dclements@protonmail.com>
2019-03-12 13:11:40 -0400

    Times completed

--- lib/devfest_schedule/cli.rb
+++ lib/devfest_schedule/cli.rb
@@ -5,6 +5,13 @@ class DevfestSchedule::CLI
     end
 
     def welcome
-        puts "Welcome to the DevFest Schedule CLI"
+        puts "Welcome to the DevFest Talks Schedule CLI."
+        puts "Please select a talk from below to build your schedule or for a further description."
     end
+
+    ### Presents schedule. Let's user see further description.
+    ### Has user select which talks to go to depending on time block.
+    ### If user's schedule hash already has block inside. Warns user there is already a talk selected.
+    ### Gives user option to replace talk with new selection.
+
 end
--- lib/devfest_schedule/talk.rb
+++ lib/devfest_schedule/talk.rb
@@ -1,16 +1,16 @@
-#class DevfestSchedule::Talk
+class DevfestSchedule::Talk
+#class Talk
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-class Talks
     attr_accessor :title, :speaker, :description, :start_time, :end_time, :session_id
     @@all = []
     
     def initialize
         @@all << self
     end
-
+### Title, Speaker, & Session ID (For Description comparison to assign)
     def self.scrape_talks   
         schedule = Nokogiri::HTML(open("https://devfest.gdgcapitalregion.com/schedule/"))
         schedule.css("div.slot.col-md-4.col-xs-12.flexbox-item-height").each do |element|  
@@ -20,38 +20,47 @@ class Talks
                 talk.speaker = element.css("p.speaker-name").text 
                 if element.attribute('id') != nil
                     session_id = element.attribute('id').value
-                    ## Grab only digits - Not gsub method
                     talk.session_id = session_id.gsub(/\D/, "")
                 end
-             end
+            end
+### Descriptions
            descriptions_modal = schedule.search("div.modals")
         
            descriptions_modal.css("div.modal.fade.text-left.people-modal").each do |modal|
 
            if modal.attribute('id') != nil
-               ## Need torefactor @@all to class method
-               @@all.each do |object|
-                if description_id = modal.attribute('id').value.gsub(/\D/, "") === talk.session_id
-                    talk.description = modal.css("p.theme-description").text
-                end
+               ## Need to refactor @@all to method
+                @@all.each do |object|
+                ## Recheck
+                    if description_id = modal.attribute('id').value.gsub(/\D/, "") === talk.session_id
+                        talk.description = modal.css("p.theme-description").text
+                    end
                end
            end
        end
-            ## Instance tests
-            #binding.pry
-           talk
-        end    
-        ## Test @@all
-         binding.pry  
-    end
-    
-## Testing method by calling
-scrape_talks
-end
-
-### Session detail label = same for description && speaker session
 ### Times: 
-    # schedule.search("")
-    # @schedule.css("div.timeslot").each do |element| 
-    # talk.start_time = (element.css("time.start-time").attribute("datetime").value).gsub('T', " ")
-    # talk.end_time = (element.css("time.end-time").attribute("datetime").value).gsub('T', " ")
+    time_parse = schedule.search("div.schedule-table.col-lg-8.col-md-10.col-md-offset-1")
+    time_parse.css("div.timeslot").each do |time| 
+        if  time.css("h5.slot-title").text != nil
+    #binding.pry
+        ## Need to refactor @@all to method
+        @@all.each do |object|
+                time.css("h5.slot-title").each do |slot_title|
+                    if slot_title.text === talk.title
+                        talk.start_time = (time.css("time.start-time").attribute("datetime").value).gsub('T', " ")
+                        talk.end_time = (time.css("time.end-time").attribute("datetime").value).gsub('T', " ")
+                    end
+                end
+            end 
+       end
+    end
+## Instance tests
+    #binding.pry
+    talk
+        end    
+## Test @@all
+        #binding.pry  
+    end   
+## Testing method by calling
+    scrape_talks
+end
