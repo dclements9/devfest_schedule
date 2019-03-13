f7ea933fcc1805592d424c369a3c85d19833d9b3
dylanclements <dclements@protonmail.com>
2019-03-13 12:35:23 -0400

    CLI Display schedule & Description

--- lib/devfest_schedule/cli.rb
+++ lib/devfest_schedule/cli.rb
@@ -1,17 +1,70 @@
 class DevfestSchedule::CLI
 
+    @@display_array = []
+    @@description_array = []
+    ## Test for now. Will be hash {time:talk}
+    @@user_schedule = []
+
     def call
         welcome
     end
 
     def welcome
         puts "Welcome to the DevFest Talks Schedule CLI."
-        puts "Please select a talk from below to build your schedule or for a further description."
+        puts "Please select a talk from below to build your schedule and for further description."
+        # Displays Time, Title, By Speaker, & Option for further description(Next Level).
+        display_dev_schedule 
     end
 
-    ### Presents schedule. Let's user see further description.
-    ### Has user select which talks to go to depending on time block.
-    ### If user's schedule hash already has block inside. Warns user there is already a talk selected.
-    ### Gives user option to replace talk with new selection.
+    def display_dev_schedule
+        ## TODO: Seperate display from rest of method in seperate method.
+        DevfestSchedule::Talk.all.each do |talk|
+            @@display_array << "#{talk.start_time} -#{talk.end_time} - #{talk.title} By: #{talk.speaker}"
+            @@description_array << "#{talk.description}"
+        end
+        ## Displays array values with index 
+        @@display_array.each.with_index(1) {|talk, i| puts "#{i}. #{talk} "}
+
+        puts "Select which talk you would like more information on by number:"
+        talk_selection_input = (gets.strip).to_i
+
+        ### Test for working. Need seperate method?
+        if talk_selection_input.to_i >= 1 && talk_selection_input.to_i < 18
+            puts "Description: #{@@description_array[talk_selection_input-1]}"
+            puts "Would you like to put this in your schedule? (y/n)"
+            entry_input = gets.strip.downcase
+            if entry_input == "y"
+                @@user_schedule << @@display_array[talk_selection_input-1]
+
+
+                # @@user_schedule.map do |time_slot, talk|
+                #     ## Possibly need talk selection to be hash?
+                #     if time_slot == ?
+                #         talk = @@display_array[talk_selection_input-1]
+                #     end
+                # end
+
+
+                puts @@user_schedule
+            elsif entry_input == "n"
+                puts "Okay. Returning to menu."
+                
+                display_dev_schedule
+            else
+                "Invalid entry. Returning to menu."
+                # need better return to menu
+                display_dev_schedule
+            end
+        else 
+            puts "Not a valid entry. Please try again."
+            # need better return to menu
+            display_dev_schedule
+        end
+    end
+
+    ## Presents devfest schedule. Let's user see further description.
+    ## Has user select which talks to go to depending on time block.
+    ## If user's schedule hash already has block inside. Warns user there is already a talk selected.
+    ## Gives user option to replace talk with new selection.
 
 end
--- lib/devfest_schedule/talk.rb
+++ lib/devfest_schedule/talk.rb
@@ -10,11 +10,16 @@ require 'open-uri'
     def initialize
         @@all << self
     end
+
+    def self.all
+        @@all
+    end
 ### Title, Speaker, & Session ID (For Description comparison to assign)
     def self.scrape_talks   
         schedule = Nokogiri::HTML(open("https://devfest.gdgcapitalregion.com/schedule/"))
         schedule.css("div.slot.col-md-4.col-xs-12.flexbox-item-height").each do |element|  
             talk = self.new
+            ## TODO: Filter "No Speaker"
             if element.css("h5.slot-title").text != ""     
                 talk.title = element.css("h5.slot-title").text
                 talk.speaker = element.css("p.speaker-name").text 
@@ -29,10 +34,8 @@ require 'open-uri'
            descriptions_modal.css("div.modal.fade.text-left.people-modal").each do |modal|
 
            if modal.attribute('id') != nil
-               ## Need to refactor @@all to method
-                @@all.each do |object|
-                ## Recheck
-                    if description_id = modal.attribute('id').value.gsub(/\D/, "") === talk.session_id
+                all.each do |object|
+                    if modal.attribute('id').value.gsub(/\D/, "") === talk.session_id
                         talk.description = modal.css("p.theme-description").text
                     end
                end
@@ -42,13 +45,11 @@ require 'open-uri'
     time_parse = schedule.search("div.schedule-table.col-lg-8.col-md-10.col-md-offset-1")
     time_parse.css("div.timeslot").each do |time| 
         if  time.css("h5.slot-title").text != nil
-    #binding.pry
-        ## Need to refactor @@all to method
-        @@all.each do |object|
+            all.each do |object|
                 time.css("h5.slot-title").each do |slot_title|
                     if slot_title.text === talk.title
-                        talk.start_time = (time.css("time.start-time").attribute("datetime").value).gsub('T', " ")
-                        talk.end_time = (time.css("time.end-time").attribute("datetime").value).gsub('T', " ")
+                        talk.start_time = (time.css("time.start-time").attribute("datetime").value).gsub('October 27T', " ")
+                        talk.end_time = (time.css("time.end-time").attribute("datetime").value).gsub('October 27T', " ")
                     end
                 end
             end 
@@ -59,8 +60,8 @@ require 'open-uri'
     talk
         end    
 ## Test @@all
-        #binding.pry  
-    end   
+        #binding.pry 
+    end  
 ## Testing method by calling
     scrape_talks
 end
